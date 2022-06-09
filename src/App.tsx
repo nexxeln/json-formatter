f294ffd5ff34306b401e4f53302d89779db35ea9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-09 20:08:14 +0530

    init with solid and unocss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="bg-true-gray-900 h-screen w-screen text-white font-sans">
+      <h1 class="text-center">JSON Formatter</h1>
+    </div>
+  );
+};
+
+export default App;

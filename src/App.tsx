d94597da918e718ad97046243f4dd6cf4791dbfe
Shoubhit Dash <shoubhit2005@gmail.com>
2022-06-09 23:15:13 +0530

    create pre component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import type { Component } from "solid-js";
+import Pre from "./components/Pre";
 
 const App: Component = () => {
   return (
     <div class="bg-true-gray-900 h-screen w-screen text-white font-sans">
       <h1 class="text-center">JSON Formatter</h1>
+
+      <Pre>
+        {`
+    "ip": "2405:201:6804:4072:7d0d:4311:2771:8db4",
+    "colo": "DEL",
+    "city": "Dehradun",
+    "country": "IN",
+    "continent": "AS",
+    "latitude": "30.32200",
+    "longitude": "78.03000",
+    "postalCode": "248001",
+    "region": "Uttarakhand",
+    "regionCode": "UT",
+    "timezone": "Asia/Kolkata"
+    `}
+      </Pre>
     </div>
   );
 };
---
+++ src/components/Pre.tsx
@@ -0,0 +1,7 @@
+const Pre = (props) => {
+  return (
+    <pre {...props} class="rounded-md text-red-400 bg-dark p-5 inline-block" />
+  );
+};
+
+export default Pre;
